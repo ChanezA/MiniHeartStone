0c5117aebc25859fdccc5d24b3ab7b6083a91a58
Alan BARON <alan.baron@etu.univ-nantes.fr>
2018-11-23 12:00:20 +0100

    Class Card modified (added attributes/methods)

--- MiniHeartStone/src/com/miniheartstone/engine/Card.java
+++ MiniHeartStone/src/com/miniheartstone/engine/Card.java
@@ -1,5 +1,35 @@
 package com.miniheartstone.engine;
 
 abstract class Card {
-
+	 
+	protected int manaCost;
+	//Every effect the card has
+	protected Effect effect;
+	protected String pictureURL;
+	protected String name;
+	protected String description;
+	
+	protected Card(int manaCost, String name, String pictureURL, String description, Effect effect) {
+		this.manaCost = manaCost;
+		this.name = name;
+		this.pictureURL = pictureURL;
+		this.description = description;
+		this.effect = effect;
+	}
+	
+	Effect getEffect() {
+		return effect;
+	}
+	
+	int getManaCost() {
+		return manaCost;
+	}
+	
+	String getName() {
+		return name;
+	}
+	
+	String getDescription() {
+		return description;
+	}
 }
