72f07c8d86e4b806a5df0fd26c2a7a4f323fd77c
Alexis Pineau <alexis@paradise-city.lan>
2018-11-27 18:39:43 +0100

    Modifs de Game et Player et ajout d'un main

---
+++ MiniHeartStone/src/com/miniheartstone/test/CommandLineLauncher.java
@@ -0,0 +1,13 @@
+package com.miniheartstone.test;
+
+import com.miniheartstone.engine.*;
+
+public class CommandLineLauncher {
+
+    public static void main(String args) {
+        Player player1 = new Player();
+        Player player2 = new Player();
+        Game game = new Game();
+    }
+
+}
