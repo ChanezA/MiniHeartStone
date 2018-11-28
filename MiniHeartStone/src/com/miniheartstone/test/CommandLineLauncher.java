2af099102e86fca111311587aba9d7195ced7d50
Alexis Pineau <alexis@paradise-city.lan>
2018-11-28 11:01:50 +0100

    G remi les modif ke loui a effacez

--- MiniHeartStone/src/com/miniheartstone/test/CommandLineLauncher.java
+++ MiniHeartStone/src/com/miniheartstone/test/CommandLineLauncher.java
@@ -2,14 +2,70 @@ package com.miniheartstone.test;
 
 import com.miniheartstone.engine.*;
 
+import java.util.Scanner;
+
 public class CommandLineLauncher {
 
-    public static void main(String args) {
-        Hero hero1 = new Magus("Gandalf", 100, 10);
-        Hero hero2 = new Warrior("Boromir", 3000, 2, 3);
-        Player player1 = new Player("Gerson", hero1, 3);
-        Player player2 = new Player("Pascal", hero2, 1);
-        Game game = new Game();
+    public static void main(String[] args) {
+        System.out.println(" /----------------------\\");
+        System.out.println("  --- MiniHeartStone ---");
+        System.out.println(" \\----------------------/\n");
+        System.out.println("Jeu développé par Alan, Alexis, Chanez et Louis\n");
+
+        Player player1 = getPlayer(1);
+        Player player2 = getPlayer(2);
+
+        Game game = new Game(player1, player2);
+
+        System.out.println("\n\noO Début de la partie Oo");
+        System.out.println("\n\n" + player1.getHero().toString());
+    }
+
+    private static Player getPlayer(int i) {
+        System.out.println("\n\nEntrez le pseudo du joueur " + i + " :");
+        Scanner sc = new Scanner(System.in);
+        String input1 = sc.nextLine();
+        int input2;
+        while (true) {
+            try {
+                System.out.println("Entrez le héro que jouera " + input1);
+                System.out.println("1 : Mage, 2 : Paladin, 3 : Guerrier :");
+                input2 = sc.nextInt();
+                if (input2 < 1 || input2 > 3) {
+                    System.out.println("Veuillez entrer un entier correct svp");
+                }
+                else break;
+            }
+            catch (Exception e) {
+                System.out.println("Veuillez entrer un entier correct svp");
+            }
+        }
+
+        Hero hero = getHero(i, input2);
+
+        return new Player(input1, hero, 1);
+    }
+
+    private static Hero getHero(int i, int input) {
+        Hero ret = null;
+        switch (input) {
+            case 1 :
+                if (i == 1) ret = new Magus("Gandalf", 2000, 10);
+                else ret = new Magus("Dumbledore", 100, 300);
+                break;
+            case 2 :
+                if (i == 1) ret = new Paladin("Lancelot", 20, 2);
+                else ret = new Magus("Perceval", 80, 3);
+                break;
+            case 3 :
+                if (i == 1) ret = new Warrior("Boromir", 3000, 4, 10);
+                else ret = new Magus("Gimli", 3000, 400);
+                break;
+            default:
+                break;
+        }
+
+        return ret;
     }
 
 }
