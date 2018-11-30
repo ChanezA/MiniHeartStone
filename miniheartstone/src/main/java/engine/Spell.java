b068ca4301e8708162eae58aaf11d3d819ffe4e4
Alexis Pineau <alexis@paradise-city.lan>
2018-11-30 10:08:47 +0100

    Passage des classes persistées en public

--- miniheartstone/src/main/java/engine/Spell.java
+++ miniheartstone/src/main/java/engine/Spell.java
@@ -1,6 +1,6 @@
 package engine;
 
-abstract class Spell extends Card{
+public abstract class Spell extends Card {
     public Spell(int manaCost, String name, String pictureURL, String description, Effect effect) {
         super(manaCost, name, pictureURL, description, effect);
     }
