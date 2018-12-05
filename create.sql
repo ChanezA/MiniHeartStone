1845a5fb883d63bd106eb74ac952af905d90d27c
Alexis Pineau <alexis@paradise-city.lan>
2018-12-05 11:28:29 +0100

    Ajout des scripts sql

---
+++ create.sql
@@ -0,0 +1,8 @@
+create table Minion (
+    name VARCHAR(40),
+    description VARCHAR(200),
+    manaCost INTEGER NOT NULL,
+    attack INTEGER NOT NULL,
+    life INTEGER NOT NULL,
+    PRIMARY KEY (name)
+)
---
+++ insert.sql
@@ -0,0 +1,6 @@
+insert into Minion
+    values ('Sanglier brocheroc', 'groink', 1, 1, 1),
+    ('Soldat du compté-de-l\'or', 'bonjour je suis un soldat du compté-del\'or', 1, 1, 2),
+    ('Chevaucheur de loup', 'bonjour je suis un chevaucheur de loup', 3, 3, 1),
+    ('Chef de raid', 'bonjour je suis un chef de raid', 3, 2, 2),
+    ('Yéti noroit', 'cc', 4, 4, 5);
