c83be98bec741aeef1ce8e03b03f6b2ee3f49ca6
E15A966F <ACAC073492258>
2018-12-07 12:09:03 +0100

    ajout frontEND

---
+++ miniheartstone/src/main/java/websocket/WebSocketController.java
@@ -0,0 +1,23 @@
+package websocket;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class WebSocketController {
+
+    private final SimpMessagingTemplate template;
+
+    @Autowired
+    WebSocketController(SimpMessagingTemplate template){
+        this.template = template;
+    }
+
+    @MessageMapping("/send/message")
+    public void onReceivedMesage(String message){
+        this.template.convertAndSend("/chat",  new SimpleDateFormat("HH:mm:ss").format(new Date())+"- "+message);
+    }
+}
+
