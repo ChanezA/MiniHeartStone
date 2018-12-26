da539bfdeb6b8f44ac4fb3dd1e9ced52e154d899
LouisQ56 <2questel.louis@gmail.com>
2018-12-26 19:02:38 +0100

    lol

--- MiniHeartStone/src/test/java/HeroTest.java
+++ MiniHeartStone/src/test/java/HeroTest.java
@@ -2,14 +2,31 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-import engine.Hero;
-import engine.Paladin;
+import engine.Hero_Doublure;
+import engine.Minion;
+import engine.Paladin_Doublure;
+import engine.Card;
 
 public class HeroTest {
-	
-	@Test
-	public final void testPaladin() {
-		Hero pal = new Paladin();
-		assertTrue(pal.getHealth() == 30);
-	}
+
+    @Test
+    public final void testPaladin() {
+    	Hero_Doublure pal = new Paladin_Doublure();
+        assertTrue(pal.getHealth() == 30);
+
+        Card crd1 = pal.draw("Sanglier brocheroc");
+        assertTrue(pal.getHand().size() == 1);
+        
+        Card crd2 = pal.draw("Chef de raid");
+        assertTrue(pal.getHand().size() == 2);
+        
+        pal.invock(crd1.getCardUUID());
+        assertTrue(((Minion)crd1).getAttack() == 1);
+        
+        pal.invock(crd2.getCardUUID());
+        assertTrue(((Minion)crd2).getAttack() == 2);
+        
+        assertTrue(((Minion)crd1).getAttack() == 2);
+        
+    }
 }
