7025d97dc522805cc4772a0aa4ceef0a9842db5b
Alexis Pineau <alexis@paradise-city.lan>
2019-01-03 14:28:43 +0100

    Maj des scripts sql

--- create.sql
+++ create.sql
@@ -6,6 +6,15 @@ create table Minion (
     life INTEGER NOT NULL,
     hasTaunt BOOLEAN NOT NULL,
     hasLifeSteal BOOLEAN NOT NULL,
-    hasCharge BOOLEAN NOT NULL, 
+    hasCharge BOOLEAN NOT NULL,
+    stringURL VARCHAR(100),
+    PRIMARY KEY (name)
+)
+
+create table Spell (
+    name VARCHAR(40),
+    description VARCHAR(200),
+    manaCost INTEGER NOT NULL,
+    stringURL VARCHAR(100),
     PRIMARY KEY (name)
 )
--- insert.sql
+++ insert.sql
@@ -1,6 +1,10 @@
 insert into Minion
-    values ('Sanglier brocheroc', 'groink', 1, 1, 1),
-    ('Soldat du compté-de-l\'or', 'bonjour je suis un soldat du compté-del\'or', 1, 1, 2),
-    ('Chevaucheur de loup', 'bonjour je suis un chevaucheur de loup', 3, 3, 1),
-    ('Chef de raid', 'bonjour je suis un chef de raid', 3, 2, 2),
-    ('Yéti noroit', 'cc', 4, 4, 5);
+    values ('Sanglier brocheroc', 'groink', 1, 1, 1, NULL),
+    ('Soldat du compté-de-l\'or', 'bonjour je suis un soldat du compté-del\'or', 1, 1, 2, NULL),
+    ('Chevaucheur de loup', 'bonjour je suis un chevaucheur de loup', 3, 3, 1, NULL),
+    ('Chef de raid', 'bonjour je suis un chef de raid', 3, 2, 2, NULL),
+    ('Yéti noroit', 'cc', 4, 4, 5, NULL);
+
+insert into Spell
+    values ('Image miroir', 'je suis un spell qui invoque 2 0/2 provoc', 1, NULL),
+    ('Maîtrise du blocage', 'je suis un spell qui pioche', 3, NULL);
