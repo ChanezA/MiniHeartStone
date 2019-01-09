cba80bf2616e43fe6d69d9af8eb86b6c4a79e01d
Alexis Pineau <alexis@paradise-city.lan>
2019-01-09 11:50:07 +0100

    Ceci est un commit assez conséquent

--- miniheartstone/src/main/java/servlet/WebSocketConfiguration.java
+++ miniheartstone/src/main/java/servlet/WebSocketConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer{
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/socket")
