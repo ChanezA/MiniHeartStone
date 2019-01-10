15cffa1ebfa4090fbeff6a94c766712d085eb89e
LouisQ56 <1questel.louis@gmail.com>
2019-01-10 15:29:15 +0100

    lol

--- hs_client/hs-client/src/app/mock-heroes.ts
+++ hs_client/hs-client/src/app/mock-heroes.ts
@@ -1,7 +1,7 @@
 import { Hero } from './heroes/hero';
 
 export const HEROES: Hero[] = [
-  { name: 'Magicien', pseudo: 'Louis', difficulty: 1},
+  { name: 'Mage', pseudo: 'Louis', difficulty: 1},
   { name: 'Guerrier', pseudo: 'Chanez', difficulty: 1 },
   { name: 'Paladin', pseudo: 'Alan', difficulty: 1 },
 ];
