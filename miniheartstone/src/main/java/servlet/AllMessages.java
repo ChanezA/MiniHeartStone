6c0282f5eefa96d27da0bca223310c3f5b7f8e85
LouisQ56 <1questel.louis@gmail.com>
2019-01-11 14:12:42 +0100

    lolololol

---
+++ miniheartstone/src/main/java/servlet/AllMessages.java
@@ -0,0 +1,25 @@
+package servlet;
+
+public class AllMessages {
+	String pseudo;
+	String hero;
+	int lvl;
+	
+	AllMessages() {}
+	
+	AllMessages(String pseudo, String hero, int lvl){
+		this.pseudo = pseudo;
+		this.hero = hero;
+		this.lvl = lvl;
+	}
+	
+	public String getPseudo() {
+		return this.pseudo;
+	}
+	public String getHero() {
+		return this.hero;
+	}
+	public int getlvl() {
+		return this.lvl;
+	}
+}
--- miniheartstone/src/main/java/servlet/WebSocketController.java
+++ miniheartstone/src/main/java/servlet/WebSocketController.java
@@ -2,11 +2,17 @@ package servlet;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import engine.EngineInterface;
 
@@ -24,6 +30,26 @@ public class WebSocketController {
 	public void onReceivedMessage(String message) {
 		//this.template.convertAndSend("/chat", new SimpleDateFormat("HH:mm:ss").format(new Date())+"- "+message);
 		this.template.convertAndSend("/chat", EngineInterface.getAllHeroDescription());
+	}
+	
+	@MessageMapping("/send/iWantPlay")
+	public void onReceivedIWantPlay (@Payload AllMessages message) {
+		try {
+			String rtrn = new ObjectMapper().writeValueAsString(new Object() {
+		        @JsonProperty private UUID rtrnmess = EngineInterface.wantPlay(message.getlvl(),message.getPseudo(),message.getHero());
+		    });
+			this.template.convertAndSend("/chat/"+message.getPseudo(),rtrn);
+			
+			System.out.println("------------------");
+			System.out.println(message.getPseudo());
+			System.out.println(message.getHero());
+			System.out.println(message.getlvl());
+			System.out.println("------------------");
+		}
+		
+		catch(Exception e) {
+			System.out.print(e.getMessage());
+		}
 	}
 	
 }
